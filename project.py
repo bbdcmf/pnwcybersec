efcdbe174f04ce80d2a930aa7264719ef836d01a
bbdcmf <freder20@pnw.edu>
2022-04-19 19:35:22 -0500

    Added comments and more performance metrics

--- project.py
+++ project.py
@@ -1,40 +1,43 @@
 #import pnwcybersec.ImageClassify as ic
 import ImageClassify as ic
-import pathlib, os
+import pathlib, os, torch, gc
 import matplotlib.pyplot as plt
 from fastai.vision.models import resnet50
 from fastai.metrics import error_rate, accuracy
 from fastai.vision.all import *
 
-#####***For running on a Windows Machine***#####
+gc.collect() # Garbage collection
+torch.cuda.empty_cache() # Empty PyTorch cache
+
+#####***Uncomment these if running on Windows***#####
 #temp = pathlib.PosixPath
 #pathlib.PosixPath = pathlib.WindowsPath
-#################################################
+#####################################################
 
 path = '/run/media/bbdcmf/T7/ITS490-Project/' # Path to the project folder
-#path = 'E:/ITS490-Project/'
-exportPath = path+'github/3-21-22-resnet50-train2-pretrained-epoch_50-bs-32-98.21%.pkl' # Path to our exported model/where we will export the model
-trainPath = path+'dataset/train2/'
+exportPath = path+'github/models/3-21-22-resnet50-train2-pretrained-epoch_50-bs-32-98.21%.pkl' # If training, this is the path to where your model will be exported. If loading a trained model, this is the path to that model.
 
 #####################################***Training a new model***#####################################
 #
+#trainPath = path+'dataset/train2/'
 #dls = ic.loadData(trainPath, valid_pct=0.2, bs=32)
 #dls.valid.show_batch(max_n=8, nrows=2)
 #plt.show()
-#model = ic.trainModel(dls, resnet50, path=exportPath, epoch_ct=50, metrics=[error_rate, accuracy], pretrained=True)
+#model = ic.trainModel(dls, resnet50, path=exportPath, epoch_ct=50, metrics=[error_rate, accuracy], pretrained=False)
 #ic.confusionMatrix(model)
+#print(dls.valid)
 #
 ####################################################################################################
 
-##################################***Loading a PreTrained Model***##################################
+##################################***Loading a Trained Model***##################################
 #
 model = ic.loadModel(exportPath, cpu=False)
 #model = ic.getBestModel(cpu=False)
 #
-####################################################################################################
+#################################################################################################
 
 answered = False
-while answered == False:
+while not answered:
     should_convert = input("Does your test set need to be converted to images?[y/n] ")
     if should_convert.lower() == 'y':
         # If the files have not been converted to images yet
@@ -60,4 +63,4 @@ while answered == False:
     else:
         print("Error, you must enter either y or n")
 
-ic.predict(model, dstPath, labeled=True)
+ic.predict(model, dstPath, labeled=True, pos_lbl='malware')
